1a2235094680ea6df90b545a8d11eb196b52a530
samwilson0745 <sohamdeb0745@gmail.com>
2023-09-07 16:06:49 +0530

    few changes

--- lib/services.dart
+++ lib/services.dart
@@ -1,7 +1,17 @@
 import 'predictor_model.dart';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'dart:io';
 class ApplicationServices{
   final Dio _dio =new Dio();
+  ApplicationServices(){
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (client) {
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
+  }
   Future prediction(String path)async{
     var formData = FormData.fromMap({
       "file":await MultipartFile.fromFile(path)
